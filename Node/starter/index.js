bcf43ebb42c88f6b3b8c0b5a31828a80a1aedb03
himanshu mac book <himanshjoshi2412@gmail.com>
2023-12-31 14:29:07 +0530

    Node starter changes

--- Node/starter/index.js
+++ Node/starter/index.js
@@ -1,18 +1,26 @@
-const express = require(`express`)
-const fs = require(`fs`)
+const fs = require('fs')
+const express = require('express')
 const port = 5000
+
 const app = express()
 
-const tourData = JSON.parse(fs.readFileSync('./dev-data/data/tours.json'))
+const tourData = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'))
+
 
 app.get('/api/v2/tours',(req,res)=>{
-  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Origin","*")
   res.send(tourData)
 })
 
 app.listen(port,()=>{
-console.log(`App is running on ${port}`)
+  console.log(`App is listening to http://127.0.0.1:5000/api/v2/tours`)
 })
+
+
+
+
+
+
 
 // // const fs = require(`fs`);
 // // const express = require(`express`);
